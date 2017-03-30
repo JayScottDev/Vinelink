73b43be64cfeb6a36c6c0d7d7646138aaaa0ac93
Alex Hutchison <alexhutchison@Alexs-MacBook-Pro.local>
2017-03-29 19:30:40 -0700

    inventory harness

--- scrap.js
+++ scrap.js
@@ -3,11 +3,12 @@ const app = express();
 const soap = require('soap');
 const parseString = require('xml2js').parseString;
 const js2xmlparser = require('js2xmlparser');
-const moment = require('moment')
 
 const taxService = 'https://ws-dev.shipcompliant.com/services/1.2/TaxService.asmx?WSDL'
 
 const supplierService = 'https://ws-dev.shipcompliant.com/services/1.2/SupplierService.asmx?WSDL'
+
+const coreService = "https://ws-dev.shipcompliant.com/services/1.2/coreservice.asmx?WSDL"
 
 const user = 'metonymyws@shipcompliant.com'
 const pass = 'Password1'
@@ -18,10 +19,10 @@ const min = -725846400000
 // const age = Math.floor(Math.random() * (max - min)) + min
 var age = 0;
 
-for (var i = 0; i < 100; i++) {
-  age = Math.floor(Math.random() * (max - min)) + min
-  console.log(moment(age).format("dddd, MMMM Do YYYY, h:mm:ss a"))
-}
+// for (var i = 0; i < 100; i++) {
+//   age = Math.floor(Math.random() * (max - min)) + min
+//   console.log(moment(age).format("dddd, MMMM Do YYYY, h:mm:ss a"))
+// }
 
 
 
@@ -58,6 +59,16 @@ const supplierReq = {
   }
 }
 
+const inventoryReq = {
+  Request: {
+    Security: {
+      PartnerKey: '',
+      Password: pass,
+      Username: user
+    }
+  }
+}
+
 // IsShippingAvailable
 // GetSalesTaxRatesByAddress
 // SupplierService
@@ -65,30 +76,44 @@ const supplierReq = {
 
 
 
-soap.createClient(taxService, function(err, client) {
-   client
-   .TaxService
-   .TaxServiceSoap12
-   .GetSalesTaxRatesByAddress(taxReq, function(err, result) {
-     if (err || result.GetSalesTaxRatesByAddressResult.Errors) {
-      throw err;
-     }
-     console.log(
-       result.GetSalesTaxRatesByAddressResult.TaxRates.WineSalesTaxPercent
-     );
-   })
-});
+// soap.createClient(taxService, function(err, client) {
+//    client
+//    .TaxService
+//    .TaxServiceSoap12
+//    .GetSalesTaxRatesByAddress(taxReq, function(err, result) {
+//      if (err || result.GetSalesTaxRatesByAddressResult.Errors) {
+//       throw err;
+//      }
+//      console.log(
+//        result.GetSalesTaxRatesByAddressResult.TaxRates.WineSalesTaxPercent
+//      );
+//    })
+// });
+//
+// soap.createClient(supplierService, function(err, client) {
+//    client
+//    .SupplierService
+//    .SupplierServiceSoap12
+//    .IsShippingAvailable(supplierReq, function(err, result) {
+//      if (err || result.IsShippingAvailableResult.Errors) {
+//       throw err;
+//      }
+//      console.log(
+//        result.IsShippingAvailableResult.IsShippingAvailable
+//      );
+//    })
+// });
 
-soap.createClient(supplierService, function(err, client) {
+soap.createClient(coreService, function(err, client) {
    client
-   .SupplierService
-   .SupplierServiceSoap12
-   .IsShippingAvailable(supplierReq, function(err, result) {
-     if (err || result.IsShippingAvailableResult.Errors) {
+   .CoreService
+   .CoreServiceSoap12
+   .GetInventoryDetails(supplierReq, function(err, result) {
+     if (err || result.GetInventoryDetailsResult.Errors) {
       throw err;
      }
      console.log(
-       result.IsShippingAvailableResult.IsShippingAvailable
+       result.GetInventoryDetailsResult
      );
    })
 });
