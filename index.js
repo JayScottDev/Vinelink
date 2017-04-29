0e4564f301e5b5fd2134be9eb9df3b2602e2560c
dkoleary88 <dan@dko.io>
2017-04-29 10:37:42 -0700

    Fix up DB middleware and add logging

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const Koa = require('koa');
 const app = new Koa();
 const hbs = require('koa-hbs');
 const serve = require('koa-static');
+const logger = require('koa-logger');
 
 const bodyparser = require('koa-bodyparser');
 const Shopify = require('shopify-api-node');
@@ -14,14 +15,17 @@ const js2xmlparser = require('js2xmlparser');
 const shipcompliant = require('./shipcompliantmethods');
 const router = require('./router');
 
+// LOGGER
+app.use(logger());
+
 //CONNECT DBs
 // start postgres client
 const postgres = require('./lib/postgres');
 // start redis client
 const redis = require('./lib/redis');
 // add middleware
-app.use(postgres);
-app.use(redis);
+app.use(postgres.middleware);
+app.use(redis.middleware);
 
 //change app url to whatever ngrok gives you, also need to change the url in shopify app settings
 
@@ -38,8 +42,8 @@ app.use(hbs.middleware({
 app.use(bodyparser());
 app.use(serve('public'));
 
-app.use(router.routes);
-app.use(router.allowedMethods);
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(process.env.PORT, function () {
   console.log(`Listening on port ${process.env.PORT}...`);
