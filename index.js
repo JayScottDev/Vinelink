592e30cd1c8a1d4927038299fdde98db51845d59
Alex <ahutch006@gmail.com>
2017-06-04 11:15:47 -0700

    make webpack server environment dependent

--- index.js
+++ index.js
@@ -12,24 +12,29 @@ const bodyparser = require('koa-bodyparser');
 const soap = require('soap');
 const parseString = require('xml2js').parseString;
 const js2xmlparser = require('js2xmlparser');
-const path = require('path')
+const path = require('path');
 
 const shipcompliant = require('./shipcompliantmethods');
 const router = require('./router');
 
-
 const sg = require('./lib/email');
-const webpackDevMiddleware = require('koa-webpack-dev-middleware');
-const webpack = require('webpack');
-const config = require('./webpack.config');
 
-const compiler = webpack(config);
+console.log('NODE_ENV', process.env.NODE_ENV);
 
-const WEBPACK_CONFIG = {
-  history: true,
-  noInfo: true,
-  publicPath: '/',
-  contentBase: path.resolve(__dirname, 'dist')
+if (process.env.NODE_ENV === 'development') {
+  const webpackDevMiddleware = require('koa-webpack-dev-middleware');
+  const webpack = require('webpack');
+  const config = require('./webpack.config');
+  const compiler = webpack(config);
+
+  const WEBPACK_CONFIG = {
+    history: true,
+    noInfo: true,
+    publicPath: '/',
+    contentBase: path.resolve(__dirname, 'dist')
+  };
+
+  app.use(webpackDevMiddleware(compiler, WEBPACK_CONFIG));
 }
 
 // ERROR HANDLER
@@ -41,7 +46,7 @@ app.use(async (ctx, next) => {
     ctx.body = {
       success: false,
       status: 500,
-      data: 'Internal Server Error',
+      data: 'Internal Server Error'
     };
     console.error(e);
   }
@@ -66,14 +71,16 @@ require('./lib/shopify').registerWebhooks();
 app.use(postgres.middleware);
 app.use(redis.middleware);
 app.use(session({}, app));
-app.use(webpackDevMiddleware(compiler, WEBPACK_CONFIG))
+
 //VIEW
-app.use(hbs.middleware({
-  viewPath: `${__dirname}/views`,
-  partialsPath: `${__dirname}/views/partials`,
-  layoutsPath: `${__dirname}/views/layouts`,
-  defaultLayout: `main`,
-}));
+app.use(
+  hbs.middleware({
+    viewPath: `${__dirname}/views`,
+    partialsPath: `${__dirname}/views/partials`,
+    layoutsPath: `${__dirname}/views/layouts`,
+    defaultLayout: `main`
+  })
+);
 
 // MIDDLEWARE
 app.keys = [process.env.SESSION_KEY];
@@ -89,20 +96,18 @@ app.use(async (ctx, next) => {
     ctx.body = {
       status,
       data,
-      success: status >= 200 && status <= 299,
+      success: status >= 200 && status <= 299
     };
   };
   await next();
 });
 
-
-
 // ROUTING
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.use(async (ctx) => {
-  await send(ctx, 'index.html', { root: __dirname + '/dist' })
+app.use(async ctx => {
+  await send(ctx, 'index.html', { root: __dirname + '/dist' });
 });
 
 app.listen(process.env.PORT, function () {
